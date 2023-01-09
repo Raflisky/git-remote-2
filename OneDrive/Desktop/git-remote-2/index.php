b01ab840539bb1745bdc7a11728325e1872167e9
RafliSky <raflimaulana214@gmail.com>
2023-01-09 10:43:02 +0700

    menambaahkan file index.php

---
+++ OneDrive/Desktop/git-remote-2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIT REMOTE 2</title>
+</head>
+<body>
+
+    <h1>Rafli Maulana Yusuf Herman</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis dolorem eaque quod aspernatur ipsum tempore in fugiat odit reprehenderit itaque libero, assumenda inventore pariatur aut repellat similique dignissimos, velit incidunt perspiciatis modi expedita! Praesentium ratione temporibus ipsam tenetur necessitatibus suscipit, itaque natus omnis architecto explicabo aspernatur placeat animi soluta molestias laborum officia rem officiis nobis eos! Quia magnam sequi excepturi ducimus iste beatae iusto tenetur doloribus nam. Vitae reprehenderit reiciendis id ea blanditiis eligendi accusantium ipsa provident vero quisquam eos nobis nisi, officia tenetur error expedita asperiores? Repudiandae iste autem eaque asperiores odio officiis repellendus maiores! Amet consectetur ea reiciendis?</p>
+
+</body>
+</html>
